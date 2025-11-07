ddf1dc7f567bb5f82a3e4e2b29dfa627b590c66a
Madi Rei <madison.nicole.rei@gmail.com>
2025-11-07 15:08:34 -0800

    adds blueprints, imports, and POST task route

--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,1 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, request
+from app.models.goal import Goal
+from ..db import db
+
+goal_bp = Blueprint("goal_bp", __name__, url_prefix="/goal")
+
