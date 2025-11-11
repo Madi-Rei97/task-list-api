87defaa8a19c206dd01fdbdee1bb4fad89d4b35a
Madi Rei <madison.nicole.rei@gmail.com>
2025-11-10 18:14:48 -0800

    refactors task routes and adds to_dict and from_dict to task model

--- app/models/task.py
+++ app/models/task.py
@@ -7,3 +7,19 @@ class Task(db.Model):
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime | None] = mapped_column(nullable=True)
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=task_data["completed_at"])
+        return new_task
+    
+    def to_dict(self):
+        task_as_dict = {}
+        task_as_dict["id"] = self.id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["completed_at"] = self.completed_at
+
+        return task_as_dict
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,32 +1,29 @@
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
 from ..db import db
+from .route_utilities import validate_model
 
-task_bp = Blueprint("task_bp", __name__, url_prefix="/task")
+bp = Blueprint("task_bp", __name__, url_prefix="/task")
 
 # POST
-@task_bp.post("")
+@bp.post("")
 def create_task():
     request_body = request.get_json()
-    title = request_body["title"]
-    description = request_body["description"]
-    completed_at = request_body["completed_at"]
 
-    new_task = Task(title=title, description=description, 
-                    completed_at=completed_at)
+    try:
+        new_task = Task.from_dict(request_body)
+        
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
     db.session.add(new_task)
     db.session.commit()
 
-    response = {
-        "id": new_task.id,
-        "title": new_task.title,
-        "description": new_task.description,
-        "completed_at": new_task.completed_at
-    }
-    return response, 201
+    return new_task.to_dict(), 201
 
 # GET
-@task_bp.get("")
+@bp.get("")
 def get_all_tasks():
     query = db.select(Task)
 
@@ -43,31 +40,19 @@ def get_all_tasks():
 
     tasks_response = []
     for task in task:
-        tasks_response.append(
-            {
-                "id": task.id,
-                "title": task.title,
-                "description": task.description,
-                "completed_at": task.completed_at
-            }
-        )
+        tasks_response.append(task.to_dict())
     return tasks_response
 
-@task_bp.get("/<task_id>")
+@bp.get("/<task_id>")
 def get_one_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(Task, task_id)
 
-    return {
-        "id": task.id,
-        "title": task.title,
-        "description": task.description,
-        "completed_at": task.completed_at
-    }
+    return task.to_dict()
 
 # PUT
-@task_bp.put("/<task_id>")
+@bp.put("/<task_id>")
 def update_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(task_id)
     request_body = request.get_json()
 
     task.title = request_body["title"]
@@ -77,27 +62,10 @@ def update_task(task_id):
     return Response(status=204, mimetype="application/json")
 
 # DELETE
-@task_bp.delete("/<task_id>")
+@bp.delete("/<task_id>")
 def delete_task(task_id):
-    task = validate_task(task_id)
+    task = validate_model(task_id)
     db.session.delete(task)
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
-
-# Validate Task
-def validate_task(task_id):
-    try:
-        task_id = int(task_id)
-    except:
-        response = {"message": f"Task {task_id} invalid"}
-        abort(make_response(response , 400))
-
-    query = db.select(Task).where(Task.id == task_id)
-    task = db.session.scalar(query)
-    
-    if not task:
-        response = {"message": f"Task {task_id} not found"}
-        abort(make_response(response, 404))
-
-    return task
